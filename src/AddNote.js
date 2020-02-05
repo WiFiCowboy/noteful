85808e61909d356f54733d39b3a9cd81a576bb8a
Humberto Hernandez <hamburger45@gmail.com>
2020-02-04 17:04:09 -0800

    add note feature completed

--- src/AddNote.js
+++ src/AddNote.js
@@ -1,53 +1,54 @@
-import React, { Component } from 'react'
-import NoteFulContext from "./context/NoteFulContext";
+import React, { Component } from 'react';
+import NoteFulContext from './context/NoteFulContext';
 import config from './config';
 
-export default class AddNote extends Component{
+export default class AddNote extends Component {
+	static contextType = NoteFulContext;
 
-  static contextType = NoteFulContext;
+	handleAddNote = (e) => {
+		e.preventDefault();
+		const name = e.target.name.value;
+		const note = {
+			name: name,
+			content: e.target.content.value
+		};
+		console.log(name);
+		fetch(`${config.API_ENDPOINT}/notes`, {
+			method: 'POST',
+			body: JSON.stringify(note),
+			headers: {
+				authorization: `bearer ${config.API_ENDPOINT}`,
+				'content-type': 'application/json'
+			}
+		})
+			.then((res) => {
+				if (!res.ok) {
+					return res.json().then((error) => {
+						throw error;
+					});
+				}
+				return res.json();
+			})
+			.then((note) => {
+				console.log('HAF', this.context);
+				this.context.addNote(note);
+				this.props.history.push(`/note/${note.id}`);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	};
 
-  handleAddNote = e => {
-    e.preventDefault();
-    const name = e.target.name.value;
-    const note = {
-      name: name
-    };
-    console.log(name);
-    fetch(`${config.API_ENDPOINT}/notes`, {
-      method: "POST",
-      body: JSON.stringify(note),
-      headers: {
-        authorization: `bearer ${config.API_ENDPOINT}`,
-        "content-type": "application/json"
-      }
-    })
-      .then(res => {
-        if (!res.ok) {
-          return res.json().then(error => {
-            throw error;
-          });
-        }
-        return res.json();
-      })
-      .then(note => {
-        console.log("HAF", this.context);
-        this.context.addNote(note);
-        this.props.history.push(`/note/${note}`);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
-  render(){
-    return(
-      <div> 
-        <form onSubmit={e => this.handleAddNote(e)}>
-          <label>Enter Note Name</label>
-          <input name="name" type="Text" placeholder="note name"></input>
-          <button type={"submit"}>Submit</button>
-        </form>
-      </div>
-    )
-    }
+	render() {
+		return (
+			<div>
+				<form onSubmit={(e) => this.handleAddNote(e)}>
+					<label>Enter Note Name</label>
+					<input name="name" type="Text" placeholder="note name" />
+					<textarea placeholder="enter note" name="content" />
+					<button type={'submit'}>Submit</button>
+				</form>
+			</div>
+		);
+	}
 }
--- src/App.js
+++ src/App.js
@@ -1,144 +1,119 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import Folder from "./Folder";
-import Menu from "./Menu";
-import Note from "./Note";
-import Nav from "./Nav";
-import NotFound from "./NotFound";
-import MainSidebar from "./MainSidebar";
-import NoteSidebar from "./NoteSidebar";
-import NoteFulContext from "./context/NoteFulContext";
-import config from "./config";
-import AddFolder from "./AddFolder";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Folder from './Folder';
+import Menu from './Menu';
+import Note from './Note';
+import Nav from './Nav';
+import NotFound from './NotFound';
+import MainSidebar from './MainSidebar';
+import NoteSidebar from './NoteSidebar';
+import NoteFulContext from './context/NoteFulContext';
+import config from './config';
+import AddFolder from './AddFolder';
 import AddNote from './AddNote';
 
 export default class App extends Component {
-  state = {
-    notes: [],
-    folders: []
-  };
+	state = {
+		notes: [],
+		folders: []
+	};
 
-  componentDidMount() {
-    Promise.all([
-      fetch(`${config.API_ENDPOINT}/notes`),
-      fetch(`${config.API_ENDPOINT}/folders`)
-    ])
-      .then(([notesRes, foldersRes]) => {
-        if (!notesRes.ok) return notesRes.json().then(e => Promise.reject(e));
-        if (!foldersRes.ok)
-          return foldersRes.json().then(e => Promise.reject(e));
+	componentDidMount() {
+		Promise.all([ fetch(`${config.API_ENDPOINT}/notes`), fetch(`${config.API_ENDPOINT}/folders`) ])
+			.then(([ notesRes, foldersRes ]) => {
+				if (!notesRes.ok) return notesRes.json().then((e) => Promise.reject(e));
+				if (!foldersRes.ok) return foldersRes.json().then((e) => Promise.reject(e));
 
-        return Promise.all([notesRes.json(), foldersRes.json()]);
-      })
-      .then(([notes, folders]) => {
-        this.setState({ notes, folders });
-      })
-      .catch(error => {
-        console.log({ error });
-      });
-  }
+				return Promise.all([ notesRes.json(), foldersRes.json() ]);
+			})
+			.then(([ notes, folders ]) => {
+				this.setState({ notes, folders });
+			})
+			.catch((error) => {
+				console.log({ error });
+			});
+	}
 
-  // fetch notes request
-  handleDeleteNote = id => {
-    this.setState({ notes: this.state.notes.filter(note => note.id !== id) });
-  };
+	// fetch notes request
+	handleDeleteNote = (id) => {
+		this.setState({ notes: this.state.notes.filter((note) => note.id !== id) });
+	};
 
- 
+	addFolder = (folder) => {
+		this.setState({
+			folders: [ ...this.state.folders, folder ]
+		});
+	};
 
-  addFolder = folder => {
-    this.setState({
-      folders: [...this.state.folders, folder]
-    });
-  };
+	addNote = (note) => {
+		this.setState({
+			notes: [ ...this.state.notes, note ]
+		});
+	};
 
-  addNote = note => {
-    this.setState({
-      notes: [...this.state.notes, note]
-    })
-  }
+	render() {
+		const value = {
+			notes: this.state.notes,
+			folders: this.state.folders,
+			deleteNote: this.handleDeleteNote,
+			handleAddFolder: this.handleAddFolder,
+			addFolder: this.addFolder,
+			addNote: this.addNote
+		};
 
-  render() {
-    const value = {
-      notes: this.state.notes,
-      folders: this.state.folders,
-      deleteNote: this.handleDeleteNote,
-      handleAddFolder: this.handleAddFolder,
-      addFolder: this.addFolder,
-      addNote: this.addNote
-
-    };
-
-    return (
-      <NoteFulContext.Provider value={value}>
-        <div className="App">
-          <Nav />
-          <div className="sideContent">
-            <aside>
-              <Switch>
-                {["/", "/folder/:folderId"].map(path => (
-                  <Route exact key={path} path={path} component={MainSidebar} />
-                ))}
-                <Route path="/Folder" component={MainSidebar} />} />
-                <Route
-                  path="/note/:noteID"
-                  render={props => (
-                    <NoteSidebar
-                      {...props}
-                      folders={this.state.folders}
-                      notes={this.state.notes}
-                    />
-                  )}
-                />
-              </Switch>
-            </aside>
-            <main>
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Menu
-                      notes={this.state.notes}
-                      deleteNote={this.handleDeleteNote}
-                      history={props.history}
-                    />
-                  )}
-                />
-                {/* /folder/folderID using links in sidebar */}
-                <Route
-                  path="/folder/:folderID"
-                  render={props => (
-                    <Folder {...props} deleteNote={this.handleDeleteNote} />
-                  )}
-                />
-                <Route
-                  path="/note/:noteID"
-                  render={props => (
-                    <Note
-                      {...props}
-                      deleteNote={this.handleDeleteNote}
-                      notes={this.state.notes}
-                    />
-                  )}
-                />
-                <Route
-                  path="/addFolder"
-                  render={props => (
-                    <AddFolder {...props} />
-                  )}
-                />
-                <Route
-                  path="/addNote"
-                  render={props => (
-                    <AddNote {...props} />
-                  )}
-                />
-                <Route component={NotFound} />
-              </Switch>
-            </main>
-          </div>
-        </div>
-      </NoteFulContext.Provider>
-    );
-  }
+		return (
+			<NoteFulContext.Provider value={value}>
+				<div className="App">
+					<Nav />
+					<div className="sideContent">
+						<aside>
+							<Switch>
+								{[ '/', '/folder/:folderId' ].map((path) => (
+									<Route exact key={path} path={path} component={MainSidebar} />
+								))}
+								<Route path="/Folder" component={MainSidebar} />} />
+								<Route
+									path="/note/:noteID"
+									render={(props) => (
+										<NoteSidebar {...props} folders={this.state.folders} notes={this.state.notes} />
+									)}
+								/>
+							</Switch>
+						</aside>
+						<main>
+							<Switch>
+								<Route
+									exact
+									path="/"
+									render={(props) => (
+										<Menu
+											notes={this.state.notes}
+											deleteNote={this.handleDeleteNote}
+											history={props.history}
+										/>
+									)}
+								/>
+								{/* /folder/folderID using links in sidebar */}
+								<Route
+									path="/folder/:folderID"
+									render={(props) => <Folder {...props} deleteNote={this.handleDeleteNote} />}
+								/>
+								<Route
+									path="/note/:noteID"
+									render={(props) => (
+										<Note {...props} deleteNote={this.handleDeleteNote} notes={this.state.notes} />
+									)}
+								/>
+								<Route path="/addFolder" render={(props) => <AddFolder {...props} />} />
+								<Route path="/addNote" render={(props) => <AddNote {...props} />} />
+								<Route component={NotFound} />
+							</Switch>
+						</main>
+					</div>
+				</div>
+			</NoteFulContext.Provider>
+		);
+	}
 }
+
+// run the noteful server !!!!
