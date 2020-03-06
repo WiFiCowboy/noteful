b6d6251c8f757283a508f37f5976372d0fafdff7
tomilone <tonymilone.tech@gmail.com>
2020-03-05 22:53:26 -0600

    minor tweaks

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   // API_ENDPOINT: 'http://localhost:9090'
-  API_FOLDERS: '   postgres://yzjdbflaadtasx:75c12a52c62d862927492c0a04d5950cd53090d976d4b5983372c5e5e30fa9a9@ec2-52-207-93-32.compute-1.amazonaws.com:5432/d3oi1oblchqs6b/api/folders/',
-  API_NOTES: "   postgres://yzjdbflaadtasx:75c12a52c62d862927492c0a04d5950cd53090d976d4b5983372c5e5e30fa9a9@ec2-52-207-93-32.compute-1.amazonaws.com:5432/d3oi1oblchqs6b/api/notes/",
+  API_FOLDERS: '   https://git.heroku.com/morning-beyond-80675.git/api/folders/',
+  API_NOTES: "   https://git.heroku.com/morning-beyond-80675.git/api/notes/",
 }
