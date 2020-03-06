9fabcfd1cedb71f0f7424bc3997085a50db51082
tomilone <tonymilone.tech@gmail.com>
2020-03-05 23:20:08 -0600

    very angry

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   // API_ENDPOINT: 'http://localhost:9090'
   API_FOLDERS: '   https://morning-beyond-80675.herokuapp.com/api/folders/',
-  API_NOTES: "   https://morning-beyond-80675.herokuapp.com//api/notes/",
+  API_NOTES: "   https://morning-beyond-80675.herokuapp.com/api/notes/",
 }
